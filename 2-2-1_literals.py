d970c37796105b7144421a6899b4eda0c62d6da7
provasko <80074452+provasko@users.noreply.github.com>
2022-04-18 21:56:52 +0200

    Update 2-2-1_literals.py

--- 2-2-1_literals.py
+++ 2-2-1_literals.py
@@ -5,4 +5,11 @@ print(0o123)
 print(0x123)
 
 print(3E8)
-print(1E-22)
+print(1E-3)
+print(0.0000000000000000000001)
+
+print("I like \"Monty Python\"")
+print('I like "Monty Python"')
+
+print(True > False)
+print(True < False)
