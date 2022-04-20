b1cd496f84b82e0c43673af2350b18b5caeec5d3
provasko <80074452+provasko@users.noreply.github.com>
2022-04-20 22:38:38 +0200

    Update 2-4-1_variables.py

--- 2-4-1_variables.py
+++ 2-4-1_variables.py
@@ -10,3 +10,25 @@ and consequently, two different variables);
 more about this soon).
 '''
 
+# dont use ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 
+# 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 
+# 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 
+# 'with', 'yield']
+
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+
+var = "3.8.5"
+print("Python version: " + var)
+
+var = 100
+var = 200 + 300
+print(var)
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
