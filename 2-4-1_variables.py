208bd9c0dd433e114d0e23688dfb35bf888df113
provasko <80074452+provasko@users.noreply.github.com>
2022-04-20 22:26:06 +0200

    Update 2-4-1_variables.py

---
+++ 2-4-1_variables.py
@@ -0,0 +1,12 @@
+'''
+- the name of the variable must be composed of upper-case or lower-case letters, digits, 
+and the character _ (underscore)
+- the name of the variable must begin with a letter;
+- the underscore character is a letter;
+- upper- and lower-case letters are treated as different (a little differently than in the real 
+world - Alice and ALICE are the same first names, but in Python they are two different variable names, 
+and consequently, two different variables);
+- the name of the variable must not be any of Python's reserved words (the keywords - we'll explain 
+more about this soon).
+'''
+
