fca8ea2445ac4ab65eb770749f489dccec947426
provasko <80074452+provasko@users.noreply.github.com>
2022-04-21 23:41:32 +0200

    Update 2-4-1_variables.py

--- 2-4-1_variables.py
+++ 2-4-1_variables.py
@@ -10,9 +10,9 @@ and consequently, two different variables);
 more about this soon).
 '''
 
-# dont use ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 
-# 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 
-# 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 
+# dont use ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue',
+# 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import',
+# 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while',
 # 'with', 'yield']
 
 var = 1
@@ -32,3 +32,22 @@ a = 3.0
 b = 4.0
 c = (a ** 2 + b ** 2) ** 0.5
 print("c =", c)
+
+'''
+- create the variables: john, mary, and adam;
+- assign values to the variables. The values must be equal to the numbers of fruit possessed by 
+John, Mary, and Adam respectively;
+- having stored the numbers in the variables, print the variables on one line, and separate each 
+of them with a comma;
+- now create a new variable named total_apples equal to addition of the three former variables.
+print the value stored in total_apples to the console;
+- experiment with your code: create new variables, assign different values to them, and perform 
+various arithmetic operations on them (e.g., +, -, *, /, //, etc.). 
+Try to print a string and an integer together on one line, e.g., "Total number of apples:" and total_apples.
+'''
+john = 1
+mary = 3
+adam = 9
+total_apples = john + mary + adam
+print(john, mary, adam, sep=",")
+print("Total: " + str(total_apples))
