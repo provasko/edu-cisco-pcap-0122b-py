501d75268729e6bfa979360826f4adeda28e6c92
provasko <80074452+provasko@users.noreply.github.com>
2022-04-22 22:44:09 +0200

    Update 2-4-1_variables.py

--- 2-4-1_variables.py
+++ 2-4-1_variables.py
@@ -61,3 +61,18 @@ sheep = sheep + 1
 x *= 2
 sheep += 1
 
+'''
+ake a look at the examples below. Make sure you understand them all.
+
+i = i + 2 * j ⇒ i += 2 * j
+
+var = var / 2 ⇒ var /= 2
+
+rem = rem % 10 ⇒ rem %= 10
+
+j = j - (i + var + rem) ⇒ j -= (i + var + rem)
+
+x = x ** 2 ⇒ x **= 2
+'''
+
+
