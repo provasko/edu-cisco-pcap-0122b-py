7678e14cc8fcf78947fef2f6347d876ac7c89382
provasko <80074452+provasko@users.noreply.github.com>
2022-04-22 22:45:31 +0200

    Update 2-4-1_variables.py

--- 2-4-1_variables.py
+++ 2-4-1_variables.py
@@ -75,4 +75,11 @@ j = j - (i + var + rem) ⇒ j -= (i + var + rem)
 x = x ** 2 ⇒ x **= 2
 '''
 
+kilometers = 12.25
+miles = 7.38
 
+miles_to_kilometers =
+kilometers_to_miles =
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
