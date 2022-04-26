847f184bef99b9b37749dc6a57fabf84bbe63531
provasko <80074452+provasko@users.noreply.github.com>
2022-04-26 23:15:08 +0200

    Update 2-5-1_comments.py

---
+++ 2-5-1_comments.py
@@ -0,0 +1,24 @@
+# This program evaluates the hypotenuse c.
+# a and b are the lengths of the legs.
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5  # We use ** instead of square root.
+print("c =", c)
+
+# This is a test program.
+x = 1
+y = 2
+# y = y + x
+print(x + y)
+
+# CMD + / for multiple lines
+# x = 1
+# y = 2
+# x = 1
+# y = 2
+# x = 1
+# y = 2
+# x = 1
+# y = 2
+# x = 1
+# y = 2
