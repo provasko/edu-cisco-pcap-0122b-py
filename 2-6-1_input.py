fc2c8fe7a50aa98e09fcacc800db25cf594712c4
provasko <80074452+provasko@users.noreply.github.com>
2022-04-28 23:38:30 +0200

    Update 2-6-1_input.py

--- 2-6-1_input.py
+++ 2-6-1_input.py
@@ -9,3 +9,8 @@ anything = float(input("Enter a number: "))
 something = anything ** 2.0
 print(anything, "to the power of 2 is", something)
 
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+hypo = (leg_a**2 + leg_b**2) ** .5
+print("Hypotenuse length is", hypo)
+
