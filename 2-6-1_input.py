6f882140e92127c63f3a30d00e7b7768b4c6ed4c
provasko <80074452+provasko@users.noreply.github.com>
2022-05-02 22:05:23 +0200

    Update 2-6-1_input.py

--- 2-6-1_input.py
+++ 2-6-1_input.py
@@ -14,3 +14,16 @@ leg_b = float(input("Input second leg length: "))
 hypo = (leg_a**2 + leg_b**2) ** .5
 print("Hypotenuse length is", hypo)
 
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
+
+print("+" + 10 * "-" + "+")
+print(("|" + " " * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
+
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+print("Hypotenuse length is " + str((leg_a**2 + leg_b**2) ** .5))
+
