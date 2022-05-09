b45290f32e4980a5625316cc592917f3762ad591
provasko <80074452+provasko@users.noreply.github.com>
2022-05-09 22:04:06 +0200

    Update 3-1-1_decisions.py

--- 3-1-1_decisions.py
+++ 3-1-1_decisions.py
@@ -100,3 +100,17 @@ if tax < 0:
 
 tax = round(tax, 0)
 print("The tax is:", tax, "thalers")
+
+
+year = int(input("Enter a year: "))
+
+if year % 400 = 0:
+    print("Leap year")
+elif year % 100 = 0:
+    print("Common year")
+elif year % 4 = 0:
+    print("Leap year")
+else:
+    print("Common year")
+
+
