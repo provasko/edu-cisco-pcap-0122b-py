8b3a8af80400f90f3455b9fcf1a6b1f4aba51ebb
provasko <80074452+provasko@users.noreply.github.com>
2022-05-09 21:57:08 +0200

    Update 3-1-1_decisions.py

--- 3-1-1_decisions.py
+++ 3-1-1_decisions.py
@@ -87,3 +87,13 @@ else:
 
 # Print the result
 print("The larger number is:", larger_number)
+
+income = float(input("Enter the annual income: "))
+
+if income <= 85528:
+    tax = income * 0.18 - 556.02
+else:
+    tax = (income - 85528) * 0.32 + 14839.02
+
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
