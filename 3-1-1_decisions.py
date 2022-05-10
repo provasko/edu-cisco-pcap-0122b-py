0faeec2e3c8f37c9b84d9b31d16621b3f8a8fce3
provasko <80074452+provasko@users.noreply.github.com>
2022-05-10 21:42:05 +0200

    Update 3-1-1_decisions.py

--- 3-1-1_decisions.py
+++ 3-1-1_decisions.py
@@ -114,3 +114,115 @@ elif year % 4 == 0:
     print("Leap year")
 else:
     print("Common year")
+
+
+#SUMMARY
+
+# The comparison (or the so-called relational) operators are used to compare values. 
+# The table below illustrates how the comparison operators work, assuming that 
+x = 0, y = 1, z = 0
+
+x == y  # False
+x == z  # True
+
+x != y  # True
+x != z  # False
+
+x > y  # False
+y > z  # True
+
+x < y  # True
+y < z  # False
+
+x >= y  # False
+x >= z  # True
+y >= z  # True
+
+x <= y  # True
+x <= z  # True
+y <= z  # False
+
+# When you want to execute some code only if a certain condition is met, you can use a conditional statement:
+
+x = 10
+
+if x == 10:  # condition
+    print("x is equal to 10")  # Executed if the condition is True.
+
+
+x = 10
+
+if x > 5:  # condition one
+    print("x is greater than 5")  # Executed if condition one is True.
+
+if x < 10:  # condition two
+    print("x is less than 10")  # Executed if condition two is True.
+
+if x == 10:  # condition three
+    print("x is equal to 10")  # Executed if condition three is True.
+
+
+
+x = 10
+
+if x < 10:  # Condition
+    print("x is less than 10")  # Executed if the condition is True.
+
+else:
+    # Executed if the condition is False.
+    print("x is greater than or equal to 10")
+
+
+x = 10
+
+if x > 5:  # True
+    print("x > 5")
+
+if x > 8:  # True
+    print("x > 8")
+
+if x > 10:  # False
+    print("x > 10")
+
+else:
+    print("else will be executed")
+
+# Each if is tested separately. The body of else is executed if the last if is False.
+
+x = 10
+
+if x == 10:  # True
+    print("x == 10")
+
+if x > 15:  # False
+    print("x > 15")
+
+elif x > 10:  # False
+    print("x > 10")
+
+elif x > 5:  # True
+    print("x > 5")
+
+else:
+    print("else will not be executed")
+
+# If the condition for if is False, the program checks the conditions of the subsequent elif blocks - 
+# the first elif block that is True is executed. If all the conditions are False, the else block 
+# will be executed.
+
+
+# Nested
+
+x = 10
+
+if x > 5:  # True
+    if x == 6:  # False
+        print("nested: x == 6")
+    elif x == 10:  # True
+        print("nested: x == 10")
+    else:
+        print("nested: else")
+else:
+    print("else")
+
+
