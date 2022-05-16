f7af790366f4d79cf3e5a68cb3a28d50fff02c9f
provasko <80074452+provasko@users.noreply.github.com>
2022-05-16 20:17:00 +0200

    Update 3-2-1_loops.py

--- 3-2-1_loops.py
+++ 3-2-1_loops.py
@@ -8,4 +8,14 @@ while conditional_expression:
     instruction_n
 '''
 
+'''
+if you want to execute more than one statement inside one while, you must (as with if) indent 
+all the instructions in the same way;
+an instruction or set of instructions executed inside the while loop is called the loop's body;
+if the condition is False (equal to zero) as early as when it is tested for the first time, the 
+body is not executed even once (note the analogy of not having to do anything if there is nothing to do);
+the body should be able to change the condition's value, because if the condition is True at 
+the beginning, the body might run continuously to infinity - notice that doing a thing usually 
+decreases the number of things to do).
+'''
 
