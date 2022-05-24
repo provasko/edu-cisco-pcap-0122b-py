95574e99f56a4d7438a5d4c0423ddf25a5696546
provasko <80074452+provasko@users.noreply.github.com>
2022-05-24 23:10:23 +0200

    Update 3-2-1_loops.py

--- 3-2-1_loops.py
+++ 3-2-1_loops.py
@@ -313,3 +313,18 @@ for i in range(3):
 
 for i in range(6, 1, -2):
     print(i, end=" ")  # Outputs: 6, 4, 2
+
+# Create a for loop that counts from 0 to 10, and prints odd numbers to the screen. 
+
+for i in range(0, 11):
+    if i % 2 != 0:
+        print(i)
+
+# Create a while loop that counts from 0 to 10, and prints odd numbers to the screen.
+
+x = 1
+while x < 11:
+    if x % 2 != 0:
+        print(x)
+    x += 1
+
