63cfc994f294f7807e46e848197c5586f5d0f036
provasko <80074452+provasko@users.noreply.github.com>
2022-05-30 23:37:52 +0200

    Update 3-4-1_lists.py

---
+++ 3-4-1_lists.py
@@ -0,0 +1,16 @@
+'''
+We'll show you how to declare such multi-value variables. 
+We'll do this with the example we just suggested. 
+We'll write a program that sorts a sequence of numbers. 
+We won't be particularly ambitious - we'll assume that there are exactly five numbers.
+
+Let's create a variable called numbers; it's assigned with not just one number, 
+but is filled with a list consisting of five values (note: the list starts with an open square 
+bracket and ends with a closed square bracket; the space between the brackets is filled with 
+five numbers separated by commas).
+'''
+
+numbers = [10, 5, 7, 2, 1]
+
+# Indexing lists
+
