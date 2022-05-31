26beb9bf0aeb69e49a7002f273a73aa5e070d0f4
provasko <80074452+provasko@users.noreply.github.com>
2022-05-31 21:36:55 +0200

    Update 3-4-1_lists.py

--- 3-4-1_lists.py
+++ 3-4-1_lists.py
@@ -14,3 +14,66 @@ numbers = [10, 5, 7, 2, 1]
 
 # Indexing lists
 
+numbers = [10, 5, 7, 2, 1]
+print("Original list content:", numbers)  # Printing original list content.
+
+numbers[0] = 111
+print("New list content: ", numbers)  # Current list content.
+
+
+numbers = [10, 5, 7, 2, 1]
+print("Original list content:", numbers)  # Printing original list content.
+
+numbers[0] = 111
+print("\nPrevious list content:", numbers)  # Printing previous list content.
+
+numbers[1] = numbers[4]  # Copying value of the fifth element to the second.
+print("New list content:", numbers)  # Printing current list content.
+
+# Accessing list content
+
+print(numbers[0])  # Accessing the list's first element.
+
+print(numbers)  # Printing the whole list.
+
+numbers = [10, 5, 7, 2, 1]
+print("Original list content:", numbers)  # Printing original list content.
+
+numbers[0] = 111
+print("\nPrevious list content:", numbers)  # Printing previous list content.
+
+numbers[1] = numbers[4]  # Copying value of the fifth element to the second.
+print("Previous list content:", numbers)  # Printing previous list content.
+
+print("\nList length:", len(numbers))  # Printing the list's length.
+
+del numbers[1]
+print(len(numbers))
+print(numbers)
+
+numbers = [10, 5, 7, 2, 1]
+print("Original list content:", numbers)  # Printing original list content.
+
+numbers[0] = 111
+print("\nPrevious list content:", numbers)  # Printing previous list content.
+
+numbers[1] = numbers[4]  # Copying value of the fifth element to the second.
+print("Previous list content:", numbers)  # Printing previous list content.
+
+print("\nList's length:", len(numbers))  # Printing previous list length.
+
+###
+
+del numbers[1]  # Removing the second element from the list.
+print("New list's length:", len(numbers))  # Printing new list length.
+print("\nNew list content:", numbers)  # Printing current list content.
+
+###
+
+
+# Negative indices are legal
+
+numbers = [111, 7, 2, 1]
+print(numbers[-1])
+print(numbers[-2])
+
