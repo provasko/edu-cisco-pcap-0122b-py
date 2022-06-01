7d2d867263b0c85a93589624b9c1331145d5353f
provasko <80074452+provasko@users.noreply.github.com>
2022-06-01 23:00:54 +0200

    Update 3-4-1_lists.py

--- 3-4-1_lists.py
+++ 3-4-1_lists.py
@@ -77,3 +77,75 @@ numbers = [111, 7, 2, 1]
 print(numbers[-1])
 print(numbers[-2])
 
+
+# Functions vs. methods
+
+'''
+A method is a specific kind of function - it behaves like a function and looks like a function, 
+but differs in the way in which it acts, and in its invocation style.
+
+A function doesn't belong to any data - it gets data, it may create new data and it (generally) 
+produces a result.
+
+A method does all these things, but is also able to change the state of a selected entity.
+
+A method is owned by the data it works for, while a function is owned by the whole code.
+
+
+This also means that invoking a method requires some specification of the data from which 
+the method is invoked.
+
+It may sound puzzling here, but we'll deal with it in depth when we delve into object-oriented 
+programming.
+
+In general, a typical function invocation may look like this:
+'''
+
+result = function(arg)
+
+
+'''
+A typical method invocation usually looks like this:
+'''
+
+result = data.method(arg)
+
+'''
+Note: the name of the method is preceded by the name of the data which owns the method. 
+Next, you add a dot, followed by the method name, and a pair of parenthesis enclosing the arguments.
+
+The method will behave like a function, but can do something more - it can change the internal 
+state of the data from which it has been invoked.
+'''
+
+# Adding elements to a list: append() and insert()
+
+list.append(value)
+
+'''
+The insert() method is a bit smarter - it can add a new element at any place in the list, 
+not only at the end.
+'''
+
+list.insert(location, value)
+
+'''
+Add the following snippet after the last line of code in the editor:
+'''
+
+numbers.insert(1, 333)
+
+# Adding elements to a list: continued
+'''
+We've modified the snippet a bit:
+'''
+
+my_list = []  # Creating an empty list.
+
+for i in range(5):
+    my_list.insert(0, i + 1)
+
+print(my_list)
+
+# Making use of lists
+
