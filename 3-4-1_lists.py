d2ae4baf77603e81de89dc3682d888bedcfe061b
provasko <80074452+provasko@users.noreply.github.com>
2022-06-03 23:22:05 +0200

    Update 3-4-1_lists.py

--- 3-4-1_lists.py
+++ 3-4-1_lists.py
@@ -235,3 +235,69 @@ we've swapped the ith element (from the beginning of the list) with the one with
 (length - i - 1) (from the end of the list); in our example, for i equal to 0 the (lenght - i - 1) 
 gives 4; for i equal to 1, it gives 3 - this is exactly what we needed.
 '''
+
+
+# Key takeaways
+
+'''
+1. The list is a type of data in Python used to store multiple objects. It is an ordered and mutable 
+collection of comma-separated items between square brackets, e.g.:
+'''
+
+my_list = [1, None, True, "I am a string", 256, 0]
+
+'''
+2. Lists can be indexed and updated, e.g.:
+'''
+
+my_list = [1, None, True, 'I am a string', 256, 0]
+print(my_list[3])  # outputs: I am a string
+print(my_list[-1])  # outputs: 0
+
+my_list[1] = '?'
+print(my_list)  # outputs: [1, '?', True, 'I am a string', 256, 0]
+
+my_list.insert(0, "first")
+my_list.append("last")
+# outputs: ['first', 1, '?', True, 'I am a string', 256, 0, 'last']
+print(my_list)
+
+'''
+3. Lists can be nested, e.g.:
+'''
+
+my_list = [1, 'a', ["list", 64, [0, 1], False]]
+
+'''
+4. List elements and lists can be deleted, e.g.:
+'''
+
+my_list = [1, 2, 3, 4]
+del my_list[2]
+print(my_list)  # outputs: [1, 2, 4]
+
+del my_list  # deletes the whole list
+
+'''
+5. Lists can be iterated through using the for loop, e.g.:
+'''
+
+my_list = ["white", "purple", "blue", "yellow", "green"]
+
+for color in my_list:
+    print(color)
+
+'''
+6. The len() function may be used to check the list's length, e.g.:
+'''
+
+my_list = ["white", "purple", "blue", "yellow", "green"]
+print(len(my_list))  # outputs 5
+
+del my_list[2]
+print(len(my_list))  # outputs 4
+
+'''
+7. A typical function invocation looks as follows: result = function(arg), while a typical 
+method invocation looks like this:result = data.method(arg).
+'''
