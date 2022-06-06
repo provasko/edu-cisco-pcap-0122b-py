b364e9859d7d73888c209bd8dd0528caaeaf8728
provasko <80074452+provasko@users.noreply.github.com>
2022-06-06 23:06:58 +0200

    Update 3-5-1_sorting.py

---
+++ 3-5-1_sorting.py
@@ -0,0 +1,24 @@
+# The bubble sort
+
+# As you can see, the essence of this algorithm is simple: we compare the adjacent elements, 
+# and by swapping some of them, we achieve our goal.
+
+my_list = [8, 10, 6, 2, 4]  # list to sort
+
+for i in range(len(my_list) - 1):  # we need (5 - 1) comparisons
+    if my_list[i] > my_list[i + 1]:  # compare adjacent elements
+        # If we end up here, we have to swap the elements.
+        my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+
+my_list = [8, 10, 6, 2, 4]  # list to sort
+swapped = True  # It's a little fake, we need it to enter the while loop.
+
+while swapped:
+    swapped = False  # no swaps so far
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            swapped = True  # a swap occurred!
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print(my_list)
