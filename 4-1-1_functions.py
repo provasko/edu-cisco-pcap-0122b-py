140c26c2371e31a41cf43a986f1a55cadd8dac2c
provasko <80074452+provasko@users.noreply.github.com>
2022-06-17 00:01:09 +0200

    Update 4-1-1_functions.py

---
+++ 4-1-1_functions.py
@@ -0,0 +1,42 @@
+def message():
+    print("Enter a value: ")
+
+print("We start here.")
+print("We end here.")
+
+
+def message():
+    print("Enter a value: ")
+
+message()
+a = int(input())
+message()
+b = int(input())
+message()
+c = int(input())
+
+
+'''
+1. A function is a block of code that performs a specific task when the function is called (invoked). 
+You can use functions to make your code reusable, better organized, and more readable. 
+Functions can have parameters and return values.
+'''
+
+'''
+2. There are at least four basic types of functions in Python:
+- built-in functions which are an integral part of Python (such as the print() function). 
+You can see a complete list of Python built-in functions at https://docs.python.org/3/library/functions.html.
+- the ones that come from pre-installed modules (you'll learn about them in the Python Essentials 
+2 course)
+- user-defined functions which are written by users for users - you can write your own functions 
+and use them freely in your code,
+- the lambda functions (you'll learn about them in the Python Essentials 2 course.)
+'''
+
+'''
+3. You can define your own function using the def keyword and the following syntax:
+'''
+
+# def your_function(optional parameters):
+    # the body of the function
+
