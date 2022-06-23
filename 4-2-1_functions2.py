a17c91cac2f1c7319ba92485d2391dd6f38a9f5f
provasko <80074452+provasko@users.noreply.github.com>
2022-06-23 23:47:51 +0200

    Update 4-2-1_functions2.py

--- 4-2-1_functions2.py
+++ 4-2-1_functions2.py
@@ -95,3 +95,61 @@ def introduction(first_name="John", last_name="Smith"):
 introduction()
 
 introduction(last_name="Hopkins")
+
+
+# Key takeaways
+
+'''
+1. You can pass information to functions by using parameters. 
+Your functions can have as many parameters as you need.
+'''
+
+
+def hi(name):
+    print("Hi,", name)
+
+hi("Greg")
+
+
+def hi_all(name_1, name_2):
+    print("Hi,", name_2)
+    print("Hi,", name_1)
+
+hi_all("Sebastian", "Konrad")
+
+'''
+2. You can pass arguments to a function using the following techniques:
+
+positional argument passing in which the order of arguments passed matters (Ex. 1),
+keyword (named) argument passing in which the order of arguments passed doesn't matter (Ex. 2),
+a mix of positional and keyword argument passing (Ex. 3).
+'''
+
+#Ex. 1
+
+def subtra(a, b):
+    print(a - b)
+
+
+subtra(5, 2)    # outputs: 3
+subtra(2, 5)    # outputs: -3
+
+
+#Ex. 2
+
+def subtra(a, b):
+    print(a - b)
+
+
+subtra(a=5, b=2)    # outputs: 3
+subtra(b=2, a=5)    # outputs: 3
+
+#Ex. 3
+
+def subtra(a, b):
+    print(a - b)
+
+
+subtra(5, b=2)    # outputs: 3
+subtra(5, 2)    # outputs: 3
+
