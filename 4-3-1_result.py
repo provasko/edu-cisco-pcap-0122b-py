d5159c847dbc2677fcb15938cd1b6c5f407fc70c
provasko <80074452+provasko@users.noreply.github.com>
2022-07-01 16:56:19 +0200

    Update 4-3-1_result.py

---
+++ 4-3-1_result.py
@@ -0,0 +1,13 @@
+def happy_new_year(wishes=True):
+    print("Three...")
+    print("Two...")
+    print("One...")
+    if not wishes:
+        return
+
+    print("Happy New Year!")
+
+
+happy_new_year()
+
+happy_new_year(False)
