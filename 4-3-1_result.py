8a9f413866daa8495244e5c23fb9760d0cbced7b
provasko <80074452+provasko@users.noreply.github.com>
2022-07-04 20:10:56 -0400

    Update 4-3-1_result.py

--- 4-3-1_result.py
+++ 4-3-1_result.py
@@ -11,3 +11,23 @@ def happy_new_year(wishes=True):
 happy_new_year()
 
 happy_new_year(False)
+
+value = None
+if value is None:
+    print("Sorry, you don't carry any value")
+
+print(strange_function(2))
+print(strange_function(1))
+
+
+def list_sum(lst):
+    s = 0
+
+    for elem in lst:
+        s += elem
+
+    return s
+
+
+print(list_sum([5, 4, 3]))
+
