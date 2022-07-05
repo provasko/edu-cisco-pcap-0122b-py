52ac49425781720bf3adc1f010f16dce33ca8347
provasko <80074452+provasko@users.noreply.github.com>
2022-07-04 20:11:45 -0400

    Update 4-3-1_result.py

--- 4-3-1_result.py
+++ 4-3-1_result.py
@@ -31,3 +31,18 @@ def list_sum(lst):
 
 print(list_sum([5, 4, 3]))
 
+# Effects and results: lists and functions
+
+
+def strange_list_fun(n):
+    strange_list = []
+
+    for i in range(0, n):
+        strange_list.insert(0, i)
+
+    return strange_list
+
+
+print(strange_list_fun(5))
+
+
