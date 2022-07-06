d2a84cbe6012b74eb903e87b8c5efb9c0484d13c
provasko <80074452+provasko@users.noreply.github.com>
2022-07-05 22:11:19 -1000

    Update 4-3-1_result.py

--- 4-3-1_result.py
+++ 4-3-1_result.py
@@ -224,3 +224,46 @@ def create_list(n):
 
 
 print(create_list(5))
+
+
+def hi():
+    return
+    print("Hi!")
+
+
+hi()
+
+
+def is_int(data):
+    if type(data) == int:
+        return True
+    elif type(data) == float:
+        return False
+
+
+print(is_int(5))
+print(is_int(5.0))
+print(is_int("5"))
+
+
+def even_num_lst(ran):
+    lst = []
+    for num in range(ran):
+        if num % 2 == 0:
+            lst.append(num)
+    return lst
+
+
+print(even_num_lst(11))
+
+
+def list_updater(lst):
+    upd_list = []
+    for elem in lst:
+        elem **= 2
+        upd_list.append(elem)
+    return upd_list
+
+
+foo = [1, 2, 3, 4, 5]
+print(list_updater(foo))
