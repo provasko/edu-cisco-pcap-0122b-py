20625f71d664a880beb3ecee14b5654c906ce078
provasko <80074452+provasko@users.noreply.github.com>
2022-07-06 22:07:51 -1000

    Update 4-4-1_scopes.py

--- 4-4-1_scopes.py
+++ 4-4-1_scopes.py
@@ -46,3 +46,50 @@ var = 1
 my_function()
 print(var)
 
+# How the function interacts with its arguments
+
+
+def my_function(my_list_1):
+    print("Print #1:", my_list_1)
+    print("Print #2:", my_list_2)
+    my_list_1 = [0, 1]
+    print("Print #3:", my_list_1)
+    print("Print #4:", my_list_2)
+
+
+my_list_2 = [2, 3]
+my_function(my_list_2)
+print("Print #5:", my_list_2)
+
+
+def my_function(my_list_1):
+    print("Print #1:", my_list_1)
+    print("Print #2:", my_list_2)
+    del my_list_1[0]  # Pay attention to this line.
+    print("Print #3:", my_list_1)
+    print("Print #4:", my_list_2)
+
+
+my_list_2 = [2, 3]
+my_function(my_list_2)
+print("Print #5:", my_list_2)
+
+'''
+if the argument is a list, then changing the value of the corresponding parameter doesn't affect 
+the list (remember: variables containing lists are stored in a different way than scalars),
+
+but if you change a list identified by the parameter (note: the list, not the parameter!), 
+the list will reflect the change.
+'''
+
+
+def my_function(n):
+    print("I got", n)
+    n += 1
+    print("I have", n)
+
+
+var = 1
+my_function(var)
+print(var)
+
