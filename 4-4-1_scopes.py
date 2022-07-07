293d8418f17f8a7d5a583315fa47ef3e089027ba
provasko <80074452+provasko@users.noreply.github.com>
2022-07-06 22:10:07 -1000

    Update 4-4-1_scopes.py

--- 4-4-1_scopes.py
+++ 4-4-1_scopes.py
@@ -93,3 +93,73 @@ var = 1
 my_function(var)
 print(var)
 
+# Key takeaways
+
+'''
+1. A variable that exists outside a function has a scope inside the function body (Example 1) 
+unless the function defines a variable of the same name (Example 2, and Example 3), e.g.:
+'''
+
+# 1
+var = 2
+
+
+def mult_by_var(x):
+    return x * var
+
+
+print(mult_by_var(7))    # outputs: 14
+
+# 2
+
+
+def mult(x):
+    var = 5
+    return x * var
+
+
+print(mult(7))    # outputs: 35
+
+# 3
+
+
+def mult(x):
+    var = 7
+    return x * var
+
+
+var = 3
+print(mult(7))    # outputs: 49
+
+
+'''
+2. A variable that exists inside a function has a scope inside the function body (Example 4), e.g.:
+
+'''
+
+
+def adding(x):
+    var = 7
+    return x + var
+
+
+print(adding(4))    # outputs: 11
+print(var)    # NameError
+
+'''
+3. You can use the global keyword followed by a variable name to make the variable's scope global, e.g.:
+'''
+
+var = 2
+print(var)    # outputs: 2
+
+
+def return_var():
+    global var
+    var = 5
+    return var
+
+
+print(return_var())    # outputs: 5
+print(var)    # outputs: 5
+
