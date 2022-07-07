c97c89b1ee209a38dddc7bfa916b07943874de6a
provasko <80074452+provasko@users.noreply.github.com>
2022-07-06 22:11:19 -1000

    Update 4-4-1_scopes.py

--- 4-4-1_scopes.py
+++ 4-4-1_scopes.py
@@ -163,3 +163,42 @@ def return_var():
 print(return_var())    # outputs: 5
 print(var)    # outputs: 5
 
+
+a = 1
+
+
+def fun():
+    a = 2
+    print(a)
+
+
+fun()
+print(a)
+
+
+a = 1
+
+
+def fun():
+    global a
+    a = 2
+    print(a)
+
+
+fun()
+a = 3
+print(a)
+
+
+a = 1
+
+
+def fun():
+    global a
+    a = 2
+    print(a)
+
+
+a = 3
+fun()
+print(a)
