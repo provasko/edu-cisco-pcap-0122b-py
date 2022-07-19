6f8dc11c0f50627eb9ac8b0111c035a9626505b3
provasko <80074452+provasko@users.noreply.github.com>
2022-07-18 22:54:11 -1000

    Update 4-6-1_tuples.py

--- 4-6-1_tuples.py
+++ 4-6-1_tuples.py
@@ -27,3 +27,31 @@ empty_tuple = ()
 one_element_tuple_1 = (1, )
 one_element_tuple_2 = 1.,
 
+# How to use a tuple?
+
+my_tuple = (1, 10, 100, 1000)
+
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[1:])
+print(my_tuple[:-2])
+
+for elem in my_tuple:
+    print(elem)
+
+# the len() function accepts tuples, and returns the number of elements contained inside
+# the + operator can join tuples together(we've shown you this already)
+# the * operator can multiply tuples, just like lists
+# the in and not in operators work in the same way as in lists.
+
+my_tuple = (1, 10, 100)
+
+t1 = my_tuple + (1000, 10000)
+t2 = my_tuple * 3
+
+print(len(t2))
+print(t1)
+print(t2)
+print(10 in my_tuple)
+print(-10 not in my_tuple)
+
