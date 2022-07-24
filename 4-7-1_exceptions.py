3fb5d4231e6ac06b85c4444a0a8ea6b3a2e281af
provasko <80074452+provasko@users.noreply.github.com>
2022-07-23 23:49:52 -1100

    Update 4-7-1_exceptions.py

--- 4-7-1_exceptions.py
+++ 4-7-1_exceptions.py
@@ -11,3 +11,20 @@ print('The reciprocal of', value, 'is', 1/value)
 
 # The try-except branch
 
+'''
+try:
+	# It's a place where
+	# you can do something
+    # without asking for permission.
+except:
+	# It's a spot dedicated to
+    # solemnly begging for forgiveness.
+'''
+
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)
+except:
+    print('I do not know what to do.')
+
+
