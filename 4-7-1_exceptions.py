490400f8d73cb6bd6a2f4fdec244e9935288b1f4
provasko <80074452+provasko@users.noreply.github.com>
2022-07-23 23:46:48 -1100

    Update 4-7-1_exceptions.py

---
+++ 4-7-1_exceptions.py
@@ -0,0 +1,13 @@
+# When data is not what it should be
+
+'''
+Traceback (most recent call last):
+  File "code.py", line 1, in 
+    value = int(input('Enter a natural number: '))
+ValueError: invalid literal for int() with base 10: ''
+'''
+value = int(input('Enter a natural number: '))
+print('The reciprocal of', value, 'is', 1/value)
+
+# The try-except branch
+
