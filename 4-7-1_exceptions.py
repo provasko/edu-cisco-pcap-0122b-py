d1298900bb30990f0fd2e3c20817350027a78ba3
provasko <80074452+provasko@users.noreply.github.com>
2022-07-23 23:51:32 -1100

    Update 4-7-1_exceptions.py

--- 4-7-1_exceptions.py
+++ 4-7-1_exceptions.py
@@ -27,4 +27,24 @@ try:
 except:
     print('I do not know what to do.')
 
+# How to deal with more than one exception
+
+'''
+Look at the code in the editor. As you can see, we've just introduced the second except branch. 
+This is not the only difference – note that both branches have exception names specified. 
+In this variant, each of the expected exceptions has its own way of handling the error, 
+but it must be emphasized that only one of all branches can intercept the control – 
+if one of the branches is executed, all the other branches remain idle.
+
+Additionally, the number of except branches is not limited – you can specify as many or as few 
+of them as you need, but don't forget that none of the exceptions can be specified more than once.
+'''
+
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)
+except ValueError:
+    print('I do not know what to do.')
+except ZeroDivisionError:
+    print('Division by zero is not allowed in our Universe.')
 
