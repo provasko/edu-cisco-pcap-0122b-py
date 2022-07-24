c93673c48cd5760a62358db43413ff5ba25a82d2
provasko <80074452+provasko@users.noreply.github.com>
2022-07-23 23:55:17 -1100

    Update 4-7-1_exceptions.py

--- 4-7-1_exceptions.py
+++ 4-7-1_exceptions.py
@@ -48,3 +48,57 @@ except ValueError:
 except ZeroDivisionError:
     print('Division by zero is not allowed in our Universe.')
 
+# The default exception and how to use it
+
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)
+except ValueError:
+    print('I do not know what to do.')
+except ZeroDivisionError:
+    print('Division by zero is not allowed in our Universe.')
+except:
+    print('Something strange has happened here... Sorry!')
+
+# ZeroDivisionError , they are: /, //, and %.
+
+# ValueError
+
+'''
+Expect this exception when you're dealing with values which may be inappropriately used in some context. 
+In general, this exception is raised when a function (like int() or float()) receives an argument 
+of a proper type, but its value is unacceptable.
+'''
+
+# TypeError
+
+'''
+This exception shows up when you try to apply a data whose type cannot be accepted in the current context. Look at the example:
+
+short_list = [1]
+one_value = short_list[0.5]
+'''
+
+# AttributeError
+
+'''
+This exception arrives – among other occasions – when you try to activate a method which doesn't exist 
+in an item you're dealing with. For example:
+
+short_list = [1]
+short_list.append(2)
+short_list.depend(3)
+'''
+
+# SyntaxError
+
+'''
+This exception is raised when the control reaches a line of code which violates Python's grammar. 
+It may sound strange, but some errors of this kind cannot be identified without first running the code. 
+This kind of behavior is typical of interpreted languages – the interpreter always works in a hurry 
+and has no time to scan the whole source code. It is content with checking the code which is 
+currently being run. An example of such a category of issues will be presented very soon.
+
+It's a bad idea to handle this exception in your programs. You should produce code that is free of 
+\syntax errors, instead of masking the faults you’ve caused.
+'''
